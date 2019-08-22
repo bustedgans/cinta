afb3904d453e6a91734df526fb1c65d171fd7285
bustedgans <50026266+bustedgans@users.noreply.github.com>
2019-08-23 05:21:04 +0700

    Add files via upload

---
+++ cinta.py
@@ -0,0 +1,101 @@
+import sys
+import os
+import time
+
+b = '\033[34;1m'
+g = '\033[32;1m'
+p = '\033[35;1m'
+c = '\033[36;1m'
+r = '\033[31;1m'
+w = '\033[37;1m'
+y = '\033[33;1m'
+
+print ('\033[31;1m[----------------Tools Perjuangan--------------]')
+print ('\033[37;1m[               Author : Insan Gans            ]')
+print ('\033[31;1m[             Team : ATTACKER NETWORK TEAM     ]')
+print ('\033[37;1m[             Contact Me : 087874439211        ]')
+print ('\033[31;1m[----------------------------------------------]')
+print('')
+
+pilihan = '''
+{1}Arti Dari Kata LDR
+
+{2}Aku Sayang Dia
+
+{3}exit'''
+time.sleep(1)
+print('')
+print (pilihan)
+gabut = input('Mau Yang Mana? : ')
+print('')
+time.sleep(1)
+
+if gabut =='1':
+    os.system('clear')
+    time.sleep(3)
+    print('\033[33;1mAda yang bilang katanya LDR itu duduk sendiri sambil nangis')
+    time.sleep(3)
+    os.system('clear')
+    print('ada yang ngomong lagi kalo ldr di selingkuhan tidak tau')
+    time.sleep(3)
+    os.system('clear')
+    print('aku gak perduli tentang itu')
+    time.sleep(3)
+    os.system('clear')
+    print('karena menurutku ldr bukan masalah singkatan tapi ldr adalah masalah tentang kepercayaan')
+    time.sleep(3)
+    os.system('clear')
+    print('ldr bukan masalah tentang bahasa tapi ldr adalah tentang perjuangan yang nyata')
+    time.sleep(3)
+    os.system('clear')
+    print('gausah di perduli ini orang yang sukanya menjatuhkan prinsipmu')
+    time.sleep(3)
+    os.system('clear')
+    print('gausah perduli orang yang suka ngatain dirimu')
+    time.sleep(3)
+    os.system('clear')
+    print('sebenarnya orang yang sukanya menjatuhkan prinsipmu dan ngatain dirimu ')
+    time.sleep(3)
+    os.system('clear')
+    print('ya itu orang yang gak bisa memiliki hati setegar dirimu')
+    time.sleep(3)
+    print('SEMANGAT LDR SLURRRR')
+
+elif gabut =='2':
+    os.system('clear')
+    time.sleep(3)
+    print('Percuma Bro Kalo Lu Sayang dia Tapi Dia Sayang Orang Lain')
+    time.sleep(3)
+    os.system('clear')
+    time.sleep(3)
+    print('Kamu Mengejar Sesuatu Yang Tak Pasti')
+    time.sleep(3)
+    os.system('clear')
+    time.sleep(3)
+    print('Terlalu Lama Memperjuangkannya Tetapi Tidak Dihargai')
+    time.sleep(3)
+    os.system('clear')
+    time.sleep(3)
+    print('Dia Bukannya Tidak Peka')
+    time.sleep(3)
+    os.system('clear')
+    time.sleep(3)
+    print('Dia Tahu Kalau Kamu Mencintainya')
+    time.sleep(3)
+    os.system('clear')
+    time.sleep(3)
+    print('Hanya Saja Dia Tidak Memperdulikanmu')
+    time.sleep(3)
+    os.system('clear')
+    time.sleep(3)
+    print('Yang Sabar Ya Bro:")')
+    time.sleep(3)
+    print('Kita Senasib:/')
+    time.sleep(2)
+
+elif gabut =='3':
+    print('Thanks To Try Tools Me:)')
+
+
+else :
+    print('wrong input!1!1!')
